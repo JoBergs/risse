1870b0752e3e300e6bfd077d891b04f6c09ed287
JoBergs <jo@knight-of-pi.org>
2019-05-30 13:18:18 -0300

    bochum xpath examples

--- risse/spiders/bochum.py
+++ risse/spiders/bochum.py
@@ -66,18 +66,48 @@ class BochumSpider(RisseSpider):
             request = scrapy.Request(response.urljoin(pdfs[i]),
                 callback=self.save_pdf)
             # DRAGONS: the filenames are not resolved like in the browser;
-            # for Linux i had to replace /
+            # for Linux i had to replace / and :
             request.meta['path'] = os.path.join(*full_path, filenames[i].replace('/', '').replace(':', '') + '.pdf')
 
             yield request
 
+    def parse_gremium(self, response):
+        """ Function to scrape all Sitzungen of a Gremium. Validates that the Sitzung
+        fits the CLI parameters and scrapes Einladung, Niederschrift and Auschuss.
+        Site:https://session.bochum.de/bi/si0041.asp?__ctopic=gr&__kgrnr=103973 """
+
+        urls, dates, niederschriften, einladungen = self.get_gremium_data(response)
+
+        for i in range(len(urls)):
+            date = dates[i].split('.')
+
+            # only parse the current sitzung url of the date fits to the parameters
+            if self.year and self.year != date[-1] or self.month and self.month != date[1].lstrip('0'):
+                continue;
+
+            path = [self.root, date[-1], response.meta['name'], '-'.join(date[::-1]), '__Dokumente']
+            self.create_directories(os.path.join(*path))
+
+            for request in self.build_gremium_requests(response,
+                                path, urls[i], einladungen[i], niederschriften[i]):
+                yield request
+
     def get_gremium_data(self, response):
+        """ Function to extract all data which is required to scrape a Gremium.
+        urls contains the Ausschuss urls, dates is necessary to see if the current Sitzung
+        fits the CLI parameters, einladungen is a list of URLs to Einladung PDFs
+        and niederschriften a list of URLs to Niederschrift PDFs. """
+        
+        # e.g. <td class="smc_td smc_field_silink"><a href="to0040.asp?__ksinr=11487" title="Details anzeigen: Bezirksvertretung Bochum-Mitte 16.05.2019 " class="smc_doc smc_datatype_si">16.05.2019</a><!--SMCINFO:si.bi.1.4.1.1.16.1.3 --> 15:00-18:09</td>
         urls = response.xpath('//tr[contains(@class, "smcrow1") or contains(@class, "smcrow2") or contains(@class, "smcrown")]/*/a/@href').getall()
         dates = response.xpath('//tr[contains(@class, "smcrow1") or contains(@class, "smcrow2") or contains(@class, "smcrown")]/*/a/text()').getall()
 
+        # e.g. <a href="getfile.asp?id=426409&amp;type=do&amp;" title="Einladung " target="_blank">Einladung <span class="smcwrapsmall smcdosize" title="Dateigröße">266\xa0KB </span></a>
         einladungen = response.xpath('//a[contains(text(), "Einladung")]/@href').getall()
+        # e.g. <a href="getfile.asp?id=427859&amp;type=do&amp;" title="Niederschrift öffentlich " target="_blank">Niederschrift öffentlich <span class="smcwrapsmall smcdosize" title="Dateigröße">570\xa0KB </span></a>
         niederschriften = response.xpath('//a[contains(text(), "Niederschrift")]/@href').getall()
 
+        # table layout in one table row; has either no, just one, or both Einladung and Niederschrift
         tables = response.xpath('//table[contains(@class, "smcdocbox smcdocboxright")]').getall()
 
         # not all einladungen have niederschriften vv. insert None accordingly
@@ -89,36 +119,23 @@ class BochumSpider(RisseSpider):
 
         return urls, dates, niederschriften, einladungen
 
-    def parse_gremium(self, response):
-        urls, dates, niederschriften, einladungen = get_gremium_data(response)
+    def build_gremium_requests(self, response, path, url, einladung, niederschrift):
+        """ For each Gremium, three requests need to be build: one request
+        for the Einladung, one request for the Niederschrift and one request
+        for parsing the Ausschuss. """
 
-        for i in range(len(urls)):
-            # it's bad to use path both as list and string
-            date = dates[i].split('.')
+        if einladung:
+            request1 = self.build_request(response.urljoin(einladung), 
+                self.save_pdf, os.path.join(*path, "Einladung.pdf"))
 
-            # only parse the current sitzung url of the date fits to the parameters
-            if self.year and self.year != date[-1] or self.month and self.month != date[1].lstrip('0'):
-                continue;
+        if niederschrift:
+            request2 = self.build_request(response.urljoin(niederschrift), 
+                self.save_pdf, os.path.join(*path, "Niederschrift_oeffentlich.pdf"))  
 
-            path = [self.root, date[-1], response.meta['name'], '-'.join(date[::-1]), '__Dokumente']
-            self.create_directories(os.path.join(*path))
+        request3 = self.build_request(response.urljoin(url), 
+            self.parse_ausschuss, path[:-1])
 
-            if einladungen[i]:
-                request = self.build_request(response.urljoin(einladungen[i]), 
-                    self.save_pdf, os.path.join(*path, "Einladung.pdf"))
-
-                yield request
-
-            if niederschriften[i]:
-                request = self.build_request(response.urljoin(niederschriften[i]), 
-                    self.save_pdf, os.path.join(*path, "Niederschrift_oeffentlich.pdf"))
-
-                yield request    
-
-            request = self.build_request(response.urljoin(urls[i]), 
-                self.parse_ausschuss, path[:-1])
-
-            yield request      
+        return [request1, request2, request3]   
 
     def parse_gremien(self, response):
         """ Find URLs for all Gremien and form a request for each of them.
